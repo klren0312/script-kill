4dc8e875b0ac9e658e722b00b7f2804bd421197c
klkl <renzw@zzes1314.cn>
2026-08-12 09:18:39 +0800

    fix: change host to 0.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const deps: GameDeps = {
 };
 
 const port = Number(process.env.PORT) || 3000;
-const host = process.env.HOST || "127.0.0.1";
+const host = process.env.HOST || "0.0.0.0";
 
 const app = await buildServer(deps);
 
